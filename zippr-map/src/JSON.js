b576d3bc921ede02f22ed93486442bf710f6c4b8
Jameela Shaik <jameelakowsar.shaik@netenrich.com>
2019-08-21 16:42:32 +0530

    map app first commit

---
+++ zippr-map/src/JSON.js
@@ -0,0 +1,231 @@
+export const jsonStructure = [
+  {
+    "City": "Kurnool",
+    "District": "Kurnool",
+    "College": "Pullareddy Engineering College",
+    "Student": "Rakesh",
+    "Joining Date": "22/2/2019"
+  },
+  {
+    "City": "Gooty",
+    "District": "Ananthapur",
+    "College": "Saint Mary Degree College",
+    "Student": "Mouli",
+    "Joining Date": "25/2/2019"
+  },
+  {
+    "City": "Allagada",
+    "District": "Kurnool",
+    "College": "Government Degree College",
+    "Student": "Ravi",
+    "Joining Date": "24/2/2019"
+  },
+  {
+    "City": "Dhone",
+    "District": "Kurnool",
+    "College": "Donbasco Engineering College",
+    "Student": "Ramesh",
+    "Joining Date": "26/2/2019"
+  },
+  {
+    "City": "Ananthapur",
+    "District": "Ananthapur",
+    "College": "Kotla Engineering College",
+    "Student": "Vikas",
+    "Joining Date": "27/2/2019"
+  },
+  {
+    "City": "Kadiri",
+    "District": "Ananthapur",
+    "College": "Government Degree College",
+    "Student": "Nani",
+    "Joining Date": "22/2/2019"
+  },
+  {
+    "City": "Nandyal",
+    "District": "Kurnool",
+    "College": "SPY Reddy Womens College",
+    "Student": "Swathi",
+    "Joining Date": "25/2/2019"
+  },
+  {
+    "City": "Kurnool",
+    "District": "Kurnool",
+    "College": "RGM Engineering College",
+    "Student": "Rekha",
+    "Joining Date": "24/2/2019"
+  },
+  {
+    "City": "Ananthapur",
+    "District": "Ananthapur",
+    "College": "Kotla Engineering College",
+    "Student": "Swapna",
+    "Joining Date": "26/2/2019"
+  },
+  {
+    "City": "Chittor",
+    "District": "Chittor",
+    "College": "Chittor Medical College",
+    "Student": "Bhaskar",
+    "Joining Date": "27/2/2019"
+  },
+  {
+    "City": "Bhimavaram",
+    "District": "West Godavari",
+    "College": "Bhimavaram Medical College",
+    "Student": "Bhavana",
+    "Joining Date": "22/2/2019"
+  },
+  {
+    "City": "Tanuku",
+    "District": "West Godavari",
+    "College": "Government Degree College",
+    "Student": "Lasya",
+    "Joining Date": "25/2/2019"
+  },
+  {
+    "City": "Rajamundry",
+    "District": "East Godavari",
+    "College": "Mary's Degree College",
+    "Student": "Bibal",
+    "Joining Date": "24/2/2019"
+  },
+  {
+    "City": "Bheemli",
+    "District": "Vishakapatnam",
+    "College": "Raghava Medical College",
+    "Student": "Karanthi",
+    "Joining Date": "26/2/2019"
+  },
+  {
+    "City": "Vishakapatnam",
+    "District": "Vishakapatnam",
+    "College": "Vishakapatnam DegreeCollege",
+    "Student": "Karthik 27/2/2019",
+    "Joining Date": ""
+  },
+  {
+    "City": "Anakapalle",
+    "District": "Vishakapatnam",
+    "College": "Anakapalle Medical College",
+    "Student": "Noor Basha",
+    "Joining Date": "22/2/2019"
+  },
+  {
+    "City": "Rajamundry",
+    "District": "East Godavari",
+    "College": "Mary's Degree College",
+    "Student": "Koteshwar",
+    "Joining Date": "25/2/2019"
+  },
+  {
+    "City": "Chittor",
+    "District": "Chittor",
+    "College": "Chittor Medical College",
+    "Student": "Mahesh",
+    "Joining Date": "24/2/2019"
+  },
+  {
+    "City": "Vishakapatnam",
+    "District": "Vishakapatnam",
+    "College": "Vishakapatnam DegreeCollege",
+    "Student": "Mohit",
+    "Joining Date": "26/2/2019"
+  },
+  {
+    "City": "Nandyal",
+    "District": "Kurnool",
+    "College": "SPY Reddy Womens College",
+    "Student": "Meghana",
+    "Joining Date": "27/2/2019"
+  },
+  {
+    "City": "Kurnool",
+    "District": "Kurnool",
+    "College": "Pullareddy Engineering College",
+    "Student": "Dany",
+    "Joining Date": "22/2/2019"
+  },
+  {
+    "City": "Kurnool",
+    "District": "Kurnool",
+    "College": "Pullareddy Engineering College",
+    "Student": "Koshal",
+    "Joining Date": "22/2/2019"
+  },
+  {
+    "City": "Kurnool",
+    "District": "Kurnool",
+    "College": "Pullareddy Engineering College",
+    "Student": "Harshini",
+    "Joining Date": "22/2/2019"
+  },
+  {
+    "City": "Allagada",
+    "District": "Kurnool",
+    "College": "Government Degree College",
+    "Student": "Kanth",
+    "Joining Date": "24/2/2019"
+  },
+  {
+    "City": "Allagada",
+    "District": "Kurnool",
+    "College": "Government Degree College",
+    "Student": "Karuna",
+    "Joining Date": "24/2/2019"
+  },
+  {
+    "City": "Allagada",
+    "District": "Kurnool",
+    "College": "Government Degree College",
+    "Student": "Raki",
+    "Joining Date": "24/2/2019"
+  },
+  {
+    "City": "Allagada",
+    "District": "Kurnool",
+    "College": "Government Degree College",
+    "Student": "Kiran",
+    "Joining Date": "24/2/2019"
+  },
+  {
+    "City": "Ananthapur",
+    "District": "Ananthapur",
+    "College": "Kotla Engineering College",
+    "Student": "Amala",
+    "Joining Date": "26/2/2019"
+  }
+];
+
+
+
+
+// City, District, College,, Student, Joining Date
+// Kurnool, Kurnool, Pullareddy Engineering College,, Rakesh, 22/2/2019
+// Gooty Ananthapur, Saint Mary Degree College,, Mouli, 25/2/2019
+// Allagada, Kurnool, Government Degree College,, Ravi, 24/2/2019
+// Dhone Kurnool, Donbasco Engineering College, Ramesh, 26/2/2019
+// Ananthapur, Ananthapur, Kotla Engineering College, Vikas, 27/2/2019
+// Kadiri, Ananthapur, Government Degree College, Nani, 22/2/2019
+// Nandyal, Kurnool, SPY Reddy Womens College, Swathi, 25/2/2019
+// Kurnool, Kurnool, RGM Engineering College, Rekha, 24/2/2019
+// Ananthapur, Ananthapur, Kotla Engineering College, Swapna, 26/2/2019
+// Chittor, Chittor, Chittor Medical College, Bhaskar, 27/2/2019
+// Bhimavaram, West Godavari, Bhimavaram Medical College, Bhavana, 22/2/2019
+// Tanuku, West Godavari, Government Degree College, Lasya, 25/2/2019
+// Rajamundry, East Godavari, Mary's Degree College, Bibal, 24/2/2019
+// Bheemli, Vishakapatnam, Raghava Medical College, Karanthi, 26/2/2019
+// Vishakapatnam, Vishakapatnam, Vishakapatnam DegreeCollege, Karthik 27/2/2019
+// Anakapalle, Vishakapatnam, Anakapalle Medical College, Noor Basha, 22/2/2019
+// Rajamundry, East Godavari, Mary's Degree College, Koteshwar, 25/2/2019
+// Chittor, Chittor, Chittor Medical College, Mahesh, 24/2/2019
+// Vishakapatnam, Vishakapatnam, Vishakapatnam DegreeCollege, Mohit, 26/2/2019
+// Nandyal, Kurnool, SPY Reddy Womens College, Meghana, 27/2/2019
+// Kurnool, Kurnool, Pullareddy Engineering College, Dany, 22/2/2019
+// Kurnool, Kurnool, Pullareddy Engineering College, Koshal, 22/2/2019
+// Kurnool, Kurnool, Pullareddy Engineering College, Harshini, 22/2/2019
+// Allagada, Kurnool, Government Degree College, Kanth, 24/2/2019
+// Allagada, Kurnool, Government Degree College, Karuna, 24/2/2019
+// Allagada, Kurnool, Government Degree College, Raki, 24/2/2019
+// Allagada, Kurnool, Government Degree College, Kiran, 24/2/2019
+// Ananthapur, Ananthapur, Kotla Engineering College, Amala, 26/2/2019
